25847342969047ba9c1a17a8df8abadb70b34364
supermuffin91 <chandler_rules@mail.ru>
2016-04-24 14:44:19 +0400

    Changed

--- Mat.cpp
+++ Mat.cpp
@@ -6,4 +6,7 @@ Dog::Dog(const std::string new_name, const int new_age)
 {
 	name = new_name; age = new_age;
 };
-void Dog::bark() {std::cout << name << std::endl;};
+void Dog::bark() {std::cout << name << std::endl << age << std::endl;};
+Dog::Dog () {name = NULL; age = 0;};
+void Dog::setName(const std::string new_name) {name = new_name;};
+void Dog::setAge (const int new_age) {age = new_age;};
